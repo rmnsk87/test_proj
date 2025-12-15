a68c95aca05dcea40a68db52d219cdef6242e47a
Roman Sky <rmnsk87@gmail.com>
2025-12-15 21:58:22 +0200

    add search icon, add start content part, link button 'start journe' to the content part

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,16 +1,17 @@
-// 1. Select elements from the DOM
 const burgerBtn = document.querySelector('.burger-menu');
 const sideMenu = document.querySelector('.side-menu');
 const closeBtn = document.querySelector('.close-btn');
 const overlay = document.querySelector('.menu-overlay');
 
-// 2. Define the toggle function
+
 function toggleMenu() {
+
     sideMenu.classList.toggle('active');
     overlay.classList.toggle('active');
 }
 
-// 3. Add Event Listeners
-burgerBtn.addEventListener('click', toggleMenu); // Click Burger -> Open
-closeBtn.addEventListener('click', toggleMenu); // Click X -> Close
-overlay.addEventListener('click', toggleMenu); // Click dark background -> Close
+/* --- EVENT LISTENERS --- */
+
+burgerBtn.addEventListener('click', toggleMenu);
+closeBtn.addEventListener('click', toggleMenu);
+overlay.addEventListener('click', toggleMenu);
